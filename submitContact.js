1330dc20b52fcc226be72ecda190a56588669238
Omkar Dhanavade <omkardhanavade2005@gmail.com>
2026-01-20 19:24:46 +0530

    Added contact form backend

---
+++ submitContact.js
@@ -0,0 +1,31 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const { name, email, message } = JSON.parse(event.body);
+
+  try {
+    await pool.query(
+      "INSERT INTO public.contacts(name, email, message) VALUES($1, $2, $3)",
+      [name, email, message]
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Message sent successfully" }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
