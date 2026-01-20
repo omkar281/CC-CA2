6858bc96c6b7c1254a0d1fe4a9f06ea27de6127c
Omkar Dhanavade <omkardhanavade2005@gmail.com>
2026-01-20 13:46:18 +0530

    Initial commit with backend

---
+++ netlify/functions/submitAdmission.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+
+  const data = JSON.parse(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Admission form submitted successfully",
+      receivedData: data,
+    }),
+  };
+};
